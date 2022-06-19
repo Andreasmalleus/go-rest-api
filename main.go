c16cc47bd81e2abc923ef8152e47b5af58939a5a
Andreasmalleus <andreasmalleus@gmail.com>
2022-06-19 20:45:39 +0300

    Database connection

--- main.go
+++ main.go
@@ -1,10 +1,38 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Andreasmalleus/go-rest-api/config"
 	"github.com/Andreasmalleus/go-rest-api/routes"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	user := os.Getenv("USER")
+	dbname := os.Getenv("DBNAME")
+
+	connString := fmt.Sprintf("postgresql://%s@localhost:5432/%s?sslmode=disable", user, dbname)
+	config.Database, err = sql.Open("postgres", connString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = config.Database.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to database")
+
 	router := routes.SetRouter()
 	router.Run(":3000")
 }
