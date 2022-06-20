9d9b4ed8ecaa3eccdcf09e53c5422fb46f805938
Andreasmalleus <andreasmalleus@gmail.com>
2022-06-20 14:47:14 +0300

    GetUser implemented

--- controllers/user.go
+++ controllers/user.go
@@ -1,10 +1,23 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Andreasmalleus/go-rest-api/config"
+	"github.com/Andreasmalleus/go-rest-api/models"
+	"github.com/gin-gonic/gin"
+)
 
 func GetUser(c *gin.Context) {
+	id := c.Param("id")
+	user := models.User{}
+	err := config.Database.QueryRow(`SELECT * FROM "user" WHERE id = $1`, id).Scan(&user.ID, &user.Name, &user.Email, &user.CreatedAt, &user.UpdatedAt)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
 	c.JSON(200, gin.H{
-		"user": "GET",
+		"status": user,
 	})
 }
 
